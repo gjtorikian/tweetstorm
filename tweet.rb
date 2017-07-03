00475893755e98a87e5821cd264b7ec0bfa71a3f
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2017-07-03 12:23:59 -0400

    faster pussycat

--- tweet.rb
+++ tweet.rb
@@ -20,7 +20,7 @@ else
   include Clockwork
   Dotenv.load
 
-  TIME = 3.minutes
+  TIME = 1.minute
 
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
