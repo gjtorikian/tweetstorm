7e80d71ec1b946c63394c754799d17cd3aecaad2
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2017-07-03 12:08:21 -0400

    A healthier amount of time

--- tweet.rb
+++ tweet.rb
@@ -20,7 +20,7 @@ else
   include Clockwork
   Dotenv.load
 
-  TIME = 10.seconds
+  TIME = 3.minutes
 
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
